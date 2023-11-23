476a72955f8fde4ff06efabf1635ed796456682e
Shoyab3811 <147497948+Shoyab3811@users.noreply.github.com>
2023-11-22 19:13:44 -1200

    My Online food

---
+++ index.php
@@ -0,0 +1,112 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Best Online Food Delivery Service in india | MyOnlineFood.com</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" media="screen and (max-width: 1200px)" href="css/rispons.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+</head>
+<body>
+    <div class="prograss"></div>
+    <nav id="navbar">
+        <div id="logo">
+            <img src="img/logo.png" alt="MyOnlineFood" width="50px" height="40px">
+        </div>
+        <ul>
+            <li class="item"><a href="#">Home</a></li>
+            <li class="item"><a href="#">Services</a></li>
+            <li class="item"><a href="#">About Us</a></li>
+            <li class="item"><a href="#">Contect Us</a></li>
+        </ul>
+        <div class="search">
+            <input type="search" id="" name="" placeholder="search here any food.....">
+            <button class="but">search</button>
+        </div>
+    </nav>
+    <section id="home">
+        <h1 class="h-primary">Welcome to MyOnlineFood</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis sed vero tenetur culpa.</p>
+        <p>aliquid modi aut architecto! Non enim iste repellat. Aut aliquam officiis in. Iste pariatur excepturi impedit repellendus aperiam. Reiciendis.</p>
+        <button class="btn">Order Now</button>
+    </section>
+    <section class="services-container">
+       <h1 class="h-primary center">Our Services</h1>
+       <div id="services">
+        <div class="box">
+            <img src="img/pija.jpg" alt="MyOnlineFood">
+            <h2 class="h-secondry center">Food Catering</h2>
+            <p class="center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, repellendus. Nesciunt fugit, nemo sapiente consectetur facere qui illum commodi vero!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. A quod temporibus autem delectus at. Pariatur dolores omnis incidunt repellat ullam culpa, sint atque? 
+            </p>
+        </div>
+        <div class="box">
+            <img src="img/pi.jpg" alt="MyOnlineFood">
+            <h2 class="h-secondry center">Bulk Ordering</h2>
+            <p class="center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, repellendus. Nesciunt fugit, nemo sapiente consectetur facere qui illum commodi vero!
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum quod expedita eius in harum eligendi iste eum. Illo adipisci sed cum alias suscipit?  
+            </p>
+        </div>
+        <div class="box">
+            <img src="img/zomato.png" alt="MyOnlineFood">
+            <h2 class="h-secondr center">Food Ordering</h2>
+            <p class="center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, repellendus. Nesciunt fugit, nemo sapiente consectetur facere qui illum commodi vero.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia minus exercitationem aperiam unde? Quam, totam laudantium. Cupiditate eius itaque possimus facilis ut quo? 
+            </p>
+        </div>
+       </div>
+    </section>
+    <section id="client-section">
+        <h1 class="h-primary center">Our Clients</h1>
+        <div id="clients">
+            <div class="client-item">
+                <img src="img/app.png" alt="MyClients">
+            </div>
+            <div class="client-item">
+                <img src="img/logo6.png" alt="MyClients">
+            </div>
+            <div class="client-item">
+                <img src="img/golo5.png" alt="MyClients">
+            </div>
+            <div class="client-item">
+                <img src="img/logo7.png" alt="MyClients">
+            </div>
+            <div class="client-item">
+                <img src="img/logo8.png" alt="MyClients">
+            </div>
+        </div>
+    </section>
+    <section id="contact">
+        <h1 class="h-primary center">Contact Us</h1>
+        <div id="contact-box">
+           <form action="" method="post">
+            <div class="form-group">
+                <label for="name">Name:</label>
+                <input type="name" name="name" id="name" placeholder="Enter your name">
+            </div>
+            <div class="form-group">
+                <label for="name">Email:</label>
+                <input type="email" name="name" id="email" placeholder="Enter your Email">
+            </div>
+            <div class="form-group">
+                <label for="name">Phone Number:</label>
+                <input type="phone" name="phone" id="" placeholder="Enter your Phone Number">
+            </div>
+            <div class="form-group">
+                <label for="name">Massage:</label>
+                <textarea name="massage" id="massage" cols="30" rows="10" placeholder="Please gine any ideas"></textarea>
+            </div>
+            <button class="btn" name="Submit">Submit Now</button>
+           </form>
+        </div>
+    </section>
+    <footer>
+       <div class="center">
+        copyright &copy; www.MyOnlineFood.com
+       </div> 
+    </footer>
+</body>
+</html>
